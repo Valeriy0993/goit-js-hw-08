6cb8f4856b2cc854f4b2a381743fbf1e3e3491da
Valeriy0993 <137831411+Valeriy0993@users.noreply.github.com>
2023-11-22 07:04:20 +0200

    Home work 8 (task 1)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+
+function renderGallery() {
+  gallery.innerHTML = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `
+    )
+    .join('');
+}
+
+renderGallery();
+
+// Використовуйте SimpleLightbox для створення галереї
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+});
 // Change code below this line
 
 console.log(galleryItems);
