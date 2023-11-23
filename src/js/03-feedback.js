818addbabc1b2181d820456f02cdb22d898fdb86
Valeriy0993 <137831411+Valeriy0993@users.noreply.github.com>
2023-11-23 21:30:26 +0200

    Home work 8 (task 3)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+  const storageKey = 'feedback-form-state';
+
+  const saveStateToLocalStorage = throttle(() => {
+    const formData = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+  }, 500);
+
+  form.addEventListener('input', saveStateToLocalStorage);
+
+  const loadStateFromLocalStorage = () => {
+    const storedData = localStorage.getItem(storageKey);
+    if (storedData) {
+      const formData = JSON.parse(storedData);
+      form.elements.email.value = formData.email || '';
+      form.elements.message.value = formData.message || '';
+    }
+  };
+
+  loadStateFromLocalStorage();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const formData = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    console.log(formData);
+
+    form.reset();
+  };
+
+  form.addEventListener('submit', handleSubmit);
+});
