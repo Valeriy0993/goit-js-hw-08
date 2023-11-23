160820d6b7a9697b06a1eaeaf6b053e3311e4c06
Valeriy0993 <137831411+Valeriy0993@users.noreply.github.com>
2023-11-23 21:44:34 +0200

    Home work 8 add code

---
+++ src/js/01-gallery.js
@@ -0,0 +1,35 @@
+// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items';
+
+const gallery = document.querySelector('.gallery');
+
+function renderGallery() {
+  gallery.innerHTML = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `
+    )
+    .join('');
+}
+
+renderGallery();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+});
+// Change code below this line
+
+console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+function loadVimeoPlayerScript(callback) {
+  const script = document.createElement('script');
+  script.src = 'https://player.vimeo.com/api/player.js';
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadVimeoPlayerScript(() => {
+    const iframe = document.getElementById('vimeo-player');
+    const player = new Vimeo.Player(iframe);
+
+    const getSavedTime = () => {
+      const savedTime = localStorage.getItem('videoplayer-current-time');
+      return savedTime !== null ? parseFloat(savedTime) : 0;
+    };
+
+    const setPlayerTime = time => {
+      player
+        .setCurrentTime(time)
+        .then(function (seconds) {})
+        .catch(function (error) {
+          switch (error.name) {
+            case 'RangeError':
+              break;
+
+            default:
+              break;
+          }
+        });
+    };
+
+    player.on('timeupdate', data => {
+      const currentTime = data.seconds;
+      updateStoredTime(currentTime);
+    });
+
+    const savedTime = getSavedTime();
+    setPlayerTime(savedTime);
+
+    const updateStoredTime = throttle(currentTime => {
+      localStorage.setItem('videoplayer-current-time', currentTime);
+    }, 1000);
+  });
+});
